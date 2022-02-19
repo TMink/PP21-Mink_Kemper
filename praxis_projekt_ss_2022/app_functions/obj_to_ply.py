1f634ceaf4cb767e7ea1c45e86fabe460c484cee
Tobias Mink <xmrmagicx@gmail.com>
2022-02-19 15:03:55 +0100

    Funktionen aufgearbeitet

--- praxis_projekt_ss_2022/app_functions/obj_to_ply.py
+++ praxis_projekt_ss_2022/app_functions/obj_to_ply.py
@@ -1,15 +1,25 @@
+import shutil
 import pymeshlab
-from app_functions.search_for_format import search_for_obj
+from app_functions.search_for_format import search_for_format
+
+OBJ_PATH = 'models/shift_coords/obj_format/'
+PLY_PATH = 'models/shift_coords/ply_format/'
 
 ms = pymeshlab.MeshSet()
 
 
 def obj_ply_convert():
-    obj_list = search_for_obj('../models/shift_coords/obj_format/schnitt_17_07')
+    obj_list = search_for_format(OBJ_PATH, ['obj'])
+    rest_list = search_for_format(OBJ_PATH, ['jpg', 'mtl'])
+
+    print(obj_list)
+    print(rest_list)
+
+    # copy all non .obj files
+    for elem in rest_list:
+        shutil.copyfile(OBJ_PATH + elem, PLY_PATH + elem)
+
     for elem in obj_list:
         print(elem)
-        ms.load_new_mesh('../models/shift_coords/schnitt_17_07/' + elem)
-        ms.save_current_mesh('../models/shift_coords/ply_format/schnitt_17_07/' + elem[:elem.rfind('.')] + '.ply')
-
-
-obj_ply_convert()
+        ms.load_new_mesh('models/shift_coords/obj_format/' + elem)
+        ms.save_current_mesh('models/shift_coords/ply_format/' + elem[:elem.rfind('.')] + '.ply')
